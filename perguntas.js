b31f3d842d11952a754ee512bc991252b7d53a26
estudantemariaeduardamenezes <00001097581536sp@al.educacao.sp.gov.br>
2024-11-26 09:12:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Piadas Ruins',
+    'O que acontece qundo chove na Inglaterra?',
+    'Vira Inglalama'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Como se chamam a pessoa que viu o Thor de perto?',
+    'Vi-thor'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Porque há uma cama elástica no Polo Norte?',
+    'Para o Urso Polar'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'A plantinha foi ao hospital, mas não foi atendida. Por que?',
+    'Porque só tinha médico de Plantão'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Quando os americanos comeram carne pela primeira vez?',
+    'Quando chegou Cristóvão “Com Lombo”'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Qual é o melhor chá para a calvíce?',
+    'Chá-peu'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Por que o médico está sempre calmo?',
+    'Porque ele tem muitos pacientes.'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Que tipo de carro o Yoda dirige?',
+    'Toyoda.'
+)
+
+criaCartao(
+    'Piadas Ruins',
+    'Como você chama uma pilha de gatinhos?',
+    'Uma Meowntanha.'
+)
