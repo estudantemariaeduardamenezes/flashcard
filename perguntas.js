af365557ddb705481c82faa2146e1b814e5546cb
estudantemariaeduardamenezes <00001097581536sp@al.educacao.sp.gov.br>
2024-11-26 09:29:00 -0300

    alterando perguntas

--- perguntas.js
+++ perguntas.js
@@ -21,33 +21,3 @@ criaCartao(
     'A plantinha foi ao hospital, mas não foi atendida. Por que?',
     'Porque só tinha médico de Plantão'
 )
-
-criaCartao(
-    'Piadas Ruins',
-    'Quando os americanos comeram carne pela primeira vez?',
-    'Quando chegou Cristóvão “Com Lombo”'
-)
-
-criaCartao(
-    'Piadas Ruins',
-    'Qual é o melhor chá para a calvíce?',
-    'Chá-peu'
-)
-
-criaCartao(
-    'Piadas Ruins',
-    'Por que o médico está sempre calmo?',
-    'Porque ele tem muitos pacientes.'
-)
-
-criaCartao(
-    'Piadas Ruins',
-    'Que tipo de carro o Yoda dirige?',
-    'Toyoda.'
-)
-
-criaCartao(
-    'Piadas Ruins',
-    'Como você chama uma pilha de gatinhos?',
-    'Uma Meowntanha.'
-)
